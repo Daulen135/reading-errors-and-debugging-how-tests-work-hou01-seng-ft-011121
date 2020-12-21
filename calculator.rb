adbfa078c0a9db88d8e499cccc8ce3b020fd5624
Daulen Zhangabylov <daulenz@gmail.com>
2020-12-21 17:05:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 def first_number, second_number)
-2.	result=(first_number.to_f+ num2.to_f)
+2.	result=(first_number.to_f+ second_number.to_f)
 3.	puts result
 4.	result
 5.	end
-6.	addition(5,4)
+6.	sum(5,4)
