cb9163a45e2fe163ad5baa70d6b57f4102595a34
Daulen Zhangabylov <daulenz@gmail.com>
2020-12-21 17:07:00 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
-def first_number, second_number)
-2.	result=(first_number.to_f+ second_number.to_f)
-3.	puts result
-4.	result
-5.	end
-6.	sum(5,4)
+def (first_number, second_number)
+result=(first_number.to_f+ second_number.to_f)
+puts result
+result
+end
+sum(5,4)
